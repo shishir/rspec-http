5c99f9c3b5b1e400533d03c204cdf7c572561387
shishir <shishir.das@gmail.com>
2014-08-02 12:19:55 +0530

    Convert specs to RSpec 2.99.1 syntax with Transpec
    This conversion is done by Transpec 2.3.6 with the following command:
        transpec
    
    * 21 conversions
        from: obj.should
          to: expect(obj).to
    
    * 9 conversions
        from: lambda { }.should
          to: expect { }.to
    
    * 9 conversions
        from: obj.should_not
          to: expect(obj).not_to
    
    For more details: https://github.com/yujinakayama/transpec#supported-conversions

--- spec/rspec/http/header_matchers_spec.rb
+++ spec/rspec/http/header_matchers_spec.rb
@@ -7,112 +7,112 @@ module RSpec::Http
 
     context "checking for the presence of a header" do
       it "passes if the header is set" do
-        response.should have_header("Content-Type")
+        expect(response).to have_header("Content-Type")
       end
 
       it "fails if the header is not set" do
-        response.should_not have_header("Foobar")
+        expect(response).not_to have_header("Foobar")
       end
 
       it "returns an appropriate failure message in the positive case" do
-        lambda {
-          response.should have_header("Foobar")
-        }.should fail_with("The header 'Foobar' was not found")
+        expect {
+          expect(response).to have_header("Foobar")
+        }.to fail_with("The header 'Foobar' was not found")
       end
 
       it "returns an appropriate failure message in the negative case" do
-        lambda {
-          response.should_not have_header("Content-Type")
-        }.should fail_with("The header 'Content-Type' should not have been found, but it was and it has a value of 'text/plain'")
+        expect {
+          expect(response).not_to have_header("Content-Type")
+        }.to fail_with("The header 'Content-Type' should not have been found, but it was and it has a value of 'text/plain'")
       end
 
       it "should be case insensitive" do
-        response.should have_header("Content-Type")
-        response.should have_header("content-type")
-        response.should have_header("Content-type")
-        response.should have_header("CONTENT-TYPE")
+        expect(response).to have_header("Content-Type")
+        expect(response).to have_header("content-type")
+        expect(response).to have_header("Content-type")
+        expect(response).to have_header("CONTENT-TYPE")
       end
     end
 
     context "checking the value of the header" do
       it "passes if the value is set correctly" do
-        response.should have_header("Content-Type" => "text/plain")
+        expect(response).to have_header("Content-Type" => "text/plain")
       end
 
       it "passes if matching is incasesensitve" do
-        response.should have_header("Content-Type" => "Text/Plain")
+        expect(response).to have_header("Content-Type" => "Text/Plain")
       end
 
       context "incorrect value" do
         it "fails if the value is incorrect" do
-          response.should_not have_header("Content-Type" => "text/csv")
+          expect(response).not_to have_header("Content-Type" => "text/csv")
         end
 
         it "returns an appropriate failure message in the positive case" do
-          lambda {
-            response.should have_header("Content-Type" => "text/csv")
-          }.should fail_with("Expected the response header 'Content-Type' to have a value of 'text/csv' but it was 'text/plain'")
+          expect {
+            expect(response).to have_header("Content-Type" => "text/csv")
+          }.to fail_with("Expected the response header 'Content-Type' to have a value of 'text/csv' but it was 'text/plain'")
         end
 
         it "returns an appropriate failure message in the negative case" do
-          lambda {
-            response.should_not have_header("Content-Type" => "text/plain")
-          }.should fail_with("Expected the response header 'Content-Type' to have a value that is not 'text/plain'")
+          expect {
+            expect(response).not_to have_header("Content-Type" => "text/plain")
+          }.to fail_with("Expected the response header 'Content-Type' to have a value that is not 'text/plain'")
         end
       end
 
       context "missing header" do
         it "fails if the header is missing" do
-          response.should_not have_header("Foobar" => "text/csv")
+          expect(response).not_to have_header("Foobar" => "text/csv")
         end
 
         it "returns an appropriate failure message in the positive case" do
-          lambda {
-            response.should have_header("Foobar" => "text/csv")
-          }.should fail_with("The header 'Foobar' was not found")
+          expect {
+            expect(response).to have_header("Foobar" => "text/csv")
+          }.to fail_with("The header 'Foobar' was not found")
         end
       end
     end
 
     context "comparing the value to a regex" do
       it "passes if the value is set correctly" do
-        response.should have_header("Content-Type" => /plain/)
+        expect(response).to have_header("Content-Type" => /plain/)
       end
 
       context "incorrect value" do
         it "fails if the value is incorrect" do
-          response.should_not have_header("Content-Type" => /csv/)
+          expect(response).not_to have_header("Content-Type" => /csv/)
         end
 
         it "returns an appropriate failure message in the positive case" do
-          lambda {
-            response.should have_header("Content-Type" => /csv/)
-          }.should fail_with("Expected the response header 'Content-Type' to have a value that matched /csv/ but it was 'text/plain'")
+          expect {
+            expect(response).to have_header("Content-Type" => /csv/)
+          }.to fail_with("Expected the response header 'Content-Type' to have a value that matched /csv/ but it was 'text/plain'")
         end
 
         it "returns an appropriate failure message in the negative case" do
-          lambda {
-            response.should_not have_header("Content-Type" => /plain/)
-          }.should fail_with("Expected the response header 'Content-Type' to have a value that does not match /plain/ but it was 'text/plain'")
+          expect {
+            expect(response).not_to have_header("Content-Type" => /plain/)
+          }.to fail_with("Expected the response header 'Content-Type' to have a value that does not match /plain/ but it was 'text/plain'")
         end
       end
 
       context "missing header" do
         it "fails if the header is missing" do
-          response.should_not have_header("Foobar" => /csv/)
+          expect(response).not_to have_header("Foobar" => /csv/)
         end
 
         it "returns an appropriate failure message in the positive case" do
-          lambda {
-            response.should have_header("Foobar" => "text/csv")
-          }.should fail_with("The header 'Foobar' was not found")
+          expect {
+            expect(response).to have_header("Foobar" => "text/csv")
+          }.to fail_with("The header 'Foobar' was not found")
         end
       end
 
       it "fails if the value is something other than a String or Regexp" do
-        lambda {
-          response.should have_header("Content-Type" => [])
-        }.should raise_error(SyntaxError, 'The value for a header should be either a String or a Regexp and not of type Array')
+        expect {
+          expect(response).to have_header("Content-Type" => [])
+        }.to raise_error(SyntaxError, 'The value for a header should be either a String or a Regexp and not of type Array')
       end
     end
   end
--- spec/rspec/http/response_code_matchers_spec.rb
+++ spec/rspec/http/response_code_matchers_spec.rb
@@ -6,19 +6,19 @@ module RSpec::Http
 
     context 'status to matcher conversion' do
       it "replaces spaces with underscores" do
-        StatusCodes.clean_up_status("Method Not Allowed").should eq(:method_not_allowed)
+        expect(StatusCodes.clean_up_status("Method Not Allowed")).to eq(:method_not_allowed)
       end
 
       it "downcases capital letters" do
-        StatusCodes.clean_up_status("IM Used").should eq(:im_used)
+        expect(StatusCodes.clean_up_status("IM Used")).to eq(:im_used)
       end
 
       it "removes apostrophes" do
-        StatusCodes.clean_up_status("I'm A Teapot").should eq(:im_a_teapot)
+        expect(StatusCodes.clean_up_status("I'm A Teapot")).to eq(:im_a_teapot)
       end
 
       it "replaces hyphens with underscores" do
-        StatusCodes.clean_up_status("Non-Authoritative Information").should eq(:non_authoritative_information)
+        expect(StatusCodes.clean_up_status("Non-Authoritative Information")).to eq(:non_authoritative_information)
       end
     end
 
@@ -26,24 +26,24 @@ module RSpec::Http
       StatusCodes.values.each do |code, status|
         it "understands if a response is of type #{status}" do
           response = Rack::MockResponse.new(code, {}, "")
-          response.should send("be_http_#{StatusCodes.as_valid_method_name(code)}")
+          expect(response).to send("be_http_#{StatusCodes.as_valid_method_name(code)}")
         end
 
         it "understands if a response is not of type #{status}" do
           response = Rack::MockResponse.new(0, {}, "")
-          response.should_not send("be_http_#{StatusCodes.as_valid_method_name(code)}")
+          expect(response).not_to send("be_http_#{StatusCodes.as_valid_method_name(code)}")
         end
       end
 
       context "where the value of the location header field can be important" do
         it "response of type created" do
           response = Rack::MockResponse.new(201, {"Location" => "http://test.server"}, "")
-          expect{ response.should be_http_ok }.to raise_error(/with a location of http:\/\/test\.server$/)
+          expect{ expect(response).to be_http_ok }.to raise_error(/with a location of http:\/\/test\.server$/)
         end
 
         it "response of type redirect" do
           response = Rack::MockResponse.new(302, {"Location" => "http://test.server"}, "")
-          expect{ response.should be_http_ok }.to raise_error(/with a location of http:\/\/test\.server$/)
+          expect{ expect(response).to be_http_ok }.to raise_error(/with a location of http:\/\/test\.server$/)
         end
       end
     end
